4f5df96955c35876f14eea926f54e9c9a2423576
rohansdevops <129587307+rohansdevops@users.noreply.github.com>
2026-01-25 21:25:30 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,4 @@
-# output "ec2_public_ip" {
-#   value = module.ec2.public_ip
-# }
-
-# output "ec2_public_dns" {
-#   value = module.ec2.public_dns
-# }
-
-
-output "ec2_public_ip" {
+output "instance_public_ip" {
   value = module.ec2.public_ip
 }
 
